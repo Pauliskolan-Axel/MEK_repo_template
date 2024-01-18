966e8125a662dcc6e8edd4c401a96abaab62b68c
axelmagnus <33330896+axelmagnus@users.noreply.github.com>
2024-01-18 14:46:56 +0100

    Create Provlab3.ino

---
+++ Provlab3/Provlab3.ino
@@ -0,0 +1,22 @@
+/* Pseudokod
+  Describe function in one or two sentences.
+ INPUT:
+ * 
+ * 
+ LOGIC:
+ * 
+ * 
+ * 
+ * OUTPUT:
+ * 
+ */
+
+//Variables: describes what it represents, and how it is used in the code
+
+void setup(){
+//Comment every row of code
+}
+
+void loop(){
+//Comment every row of code  
+}
